c8ddecf95e160fd471cb04dc026c2a05fe16e0ea
stanondieki <ondiekistanley21@gmail.com>
2024-12-02 20:24:11 +0300

    login update

---
+++ pages/login.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import LoginRegister from "@/components/Logins/LoginRegister";
+
+const LoginPage = () => {
+    return(
+        <div>
+            <LoginRegister/>
+        </div>
+    )
+};
+export default LoginPage;
